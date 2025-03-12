56b06ea36df96ed190e6f1b4e733d92d96405762
lamaquina27 <jdsanchezv27@gmail.com>
2025-03-11 19:16:53 -0500

    main 2

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.config import Config
+
 
 class MainScreen(BoxLayout):
     pass
 
 class MyApp(MDApp):
     def build(self):
+        # Opcional: Si estás en PC, fija la resolución para simular un móvil
+        Window.size = (360, 640)  # Simula un celular en modo vertical
+        Config.set('graphics', 'fullscreen', 'auto')  # Pantalla completa en móviles
+
         Builder.load_file("vista.kv")
         return MainScreen()
  
